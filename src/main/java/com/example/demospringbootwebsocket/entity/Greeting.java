1cd6c90964a0325c07798b4939103d6a69bb7c06
florentin-a-p <florentin.purnama@gmail.com>
2023-01-23 16:42:30 +0700

    init commit

---
+++ src/main/java/com/example/demospringbootwebsocket/entity/Greeting.java
@@ -0,0 +1,14 @@
+package com.example.demospringbootwebsocket.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Greeting {
+  private String content;
+}
---
+++ src/main/java/com/example/demospringbootwebsocket/entity/Message.java
@@ -0,0 +1,14 @@
+package com.example.demospringbootwebsocket.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Message {
+  private String messageContent;
+}
